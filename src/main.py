0fd57f60674787f0109663375de883e20fd2b59f
Dmitry <lakizmeye14@gmail.com>
2023-05-26 14:31:13 +0300

    Доделал фильтры

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtSql import QSqlTableModel
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QTableWidgetItem, QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QComboBox, QFileDialog, QMessageBox, QTableWidgetItem, QApplication, QMainWindow, QTableWidgetItem
 from PyQt5.QtWidgets import QTableWidgetItem, QWidget, QPushButton, QLineEdit
 
 from excel_writer import save_excel
@@ -19,11 +19,79 @@ class Tracker(QMainWindow):
         # Назначаем метод выделения по строкам и только одну ячейку
         self.ui.tableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows);
         self.ui.tableView.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection);
+        # добавление сортировки
+        self.ui.tableView.setSortingEnabled(True)
 
         # связываем сигналы и слоты
         self.ui.exchange.clicked.connect(self.process_add_or_update_button)
         self.ui.repear.clicked.connect(self.delete_current_transaction)
         self.ui.openes.clicked.connect(self.create_report)
+        # связываем слоты фильтрации
+        self.mapper = QtCore.QSignalMapper()
+        self.mapper.mapped[str].connect(self.filter_for_column)
+
+        self.ui.month_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.month_1, self.ui.month_1.objectName())
+
+        self.ui.number_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.number_1, self.ui.number_1.objectName())
+
+        self.ui.item_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.item_1, self.ui.item_1.objectName())
+
+        self.ui.group_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.group_1, self.ui.group_1.objectName())
+
+        self.ui.classes_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.classes_1, self.ui.classes_1.objectName())
+
+        self.ui.week_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.week_1, self.ui.week_1.objectName())
+
+        self.ui.who_1.currentTextChanged.connect(self.mapper.map)
+        self.mapper.setMapping(self.ui.who_1, self.ui.who_1.objectName())
+
+    @QtCore.pyqtSlot(str)
+    def filter_for_column(self, object_name):
+        """
+        Слот обрабатывает выбор ячейки в поле фильтра
+        """
+        # получаем весь список объектов
+        list_obj = self.findChildren(QComboBox)
+        # получаем объект от которого пришел сигнал
+        obj = self.findChild(QComboBox, object_name)
+        # проходимся, сбрасываем фильтры и назначаем новый
+        for ui_obj in list_obj:
+            if obj != ui_obj:
+                ui_obj.setCurrentIndex(0) # type: ignore
+        if object_name == "month_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(1)
+        elif object_name == "number_1":
+            text = "" if obj.currentText()  == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(2)
+        elif object_name == "item_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(3)
+        elif object_name == "group_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(4)
+        elif object_name == "classes_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(5)
+        elif object_name == "week_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(6)
+        elif object_name == "who_1":
+            text = "" if obj.currentText() == "Все" else obj.currentText()
+            self.model.setFilterFixedString(text)
+            self.model.setFilterKeyColumn(7)
 
     def view_data(self):
         """
@@ -32,9 +100,11 @@ class Tracker(QMainWindow):
         """
         try:
             self.conn = Data()
-            self.model = QSqlTableModel(self, self.conn.get_conn())
-            self.model.setTable('expense')
-            self.model.select()
+            self.sql_model = QSqlTableModel(self, self.conn.get_conn())
+            self.sql_model.setTable('expense')
+            self.sql_model.select()
+            self.model = QtCore.QSortFilterProxyModel()
+            self.model.setSourceModel(self.sql_model)
             self.ui.tableView.setModel(self.model)
         except FileExistsError as err:
             # В случае ошибки подключения выход с ошибкой
@@ -104,7 +174,7 @@ class Tracker(QMainWindow):
                                                group, type_lesson, week, who,
                                                index.data())
         # Обновляем модель
-        self.model.select()
+        self.sql_model.select()
 
     def insert_data_to_table(self):
         """
@@ -127,7 +197,7 @@ class Tracker(QMainWindow):
             self.conn.add_new_transaction_query(mounth, number_lesson, lesson,
                                                 group, type_lesson, week, who)
         # Обновляем модель
-        self.model.select()
+        self.sql_model.select()
 
     @QtCore.pyqtSlot()
     def delete_current_transaction(self):
@@ -153,7 +223,7 @@ class Tracker(QMainWindow):
         if returnValue == QMessageBox.Ok:
             self.conn.delete_transaction_query(index.data())
         # Обновляем модель
-        self.model.select()
+        self.sql_model.select()
 
 
 if __name__ == "__main__":
